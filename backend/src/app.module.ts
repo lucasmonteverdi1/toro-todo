796c3b048750edffa4aa2a01b2e7103e11365a01
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-08-26 12:27:36 -0300

    fix attempt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from "./models/user/user.model";
 import { Task } from "./models/task/task.model";
 import {UserModule} from "./models/user/user.module";
 import {TaskModule} from "./models/task/task.module";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {TaskController} from "./models/task/task.controller";
+import {UserController} from "./models/user/user.controller";
+import {TaskService} from "./models/task/task.service";
+import {UserService} from "./models/user/user.service";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
+      host: 'postgres', // This should match the service name in docker-compose
       port: 5432,
       username: 'postgres',
-      password: 'password',
+      password: 'password', // Make sure this matches the environment setting
       database: 'postgres',
       models: [User, Task],
       autoLoadModels: true,
@@ -21,6 +29,7 @@ import {TaskModule} from "./models/task/task.module";
     UserModule,
     TaskModule,
   ],
-  providers: [],
+  controllers: [TaskController, UserController],
+  providers: [TaskService, UserService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import { Sequelize } from 'sequelize-typescript';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const port = 3100;
+
+  const sequelize = app.get<Sequelize>(Sequelize);
+
+  await sequelize
+    .sync()
+    .then(() => console.log('Database & tables synchronized'))
+    .catch((err) => console.error('Error synchronizing database:', err));
+
+  await app.listen(port);
 }
 bootstrap();
